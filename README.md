4f264def3ce68bb854c6450b90c3b26352ebbcb5
fu.xiao <im.fu.xiao@gmail.com>
2023-11-14 22:27:52 +0800

    feat: 仓云端输入方案功能

--- README.md
+++ README.md
@@ -1,18 +1,75 @@
 # RIME输入方案
 
-* clover: 四叶草全拼, 来源: https://github.com/fkxxyz/rime-cloverpinyin (shell脚本拉取)
-<!-- * wubi86_jidian: 极点五笔, 来源: https://github.com/SivanLaai/rime-pure -->
-<!-- * wubi86_qq: QQ五笔, 来源: https://github.com/SivanLaai/rime-pure) -->
-* double_pinyin_flypy: 小鹤双拼, 来源: https://github.com/LufsX/rime
-* rime-wubi86-jidian: https://github.com/KyleBing/rime-wubi86-jidian
-* yuhao: 宇浩输入法, 来源: https://github.com/forFudan/yuhao
+感谢大家使用仓输入法，仓输入法的云端下载功能已完成。后续仓内置输入方案将迁移至云端，不会在随 App 一起发布商店。
 
-rime输入方案整理, 方案来源其他github开源项目.
+> 这里的云端是指 Apple 提供的 CloudKit。
+
+注意：之前已经内置到 App 中的方案，我会先一步推送至云端，后续请作者发邮件给我，我会为将您加入至单独维护云端方案 App 的测试列表中。
+
+# 如何上传方案至仓的云端？
+
+基于安全原因，仓输入法不会内置开源方案的上传/修改/删除功能，此功能我已开发了单独的 App，但不会上架，只会发布 TF 版本。
+
+如果您需要将自己的方案上传至云端， 目前暂行步骤是：
+
+1. Fork 本仓库，参考 `build.sh` 中已有的输入方案的脚本，并向其中添加您的输入方案。（如果这步太难，您也可以直接修改 README.md 中“云端支持方案”部分，添加您的输入方案信息）
+2. 向我的邮箱（morse.hsiao@gmail.com）发封邮件，邮件中请附上您的 iCloud 账户，我会将您的 iCloud 账户添加到 TF 版本测试员列表中。
+3. 在我添加后，您的 iCloud 对应的邮箱会有一封邀请邮件，点击链接网页会显示一个邀请码，在 TestFlight 应用中输入这个邀请码，您就可以在 TestFlight 中看到「仓输入方案管理」应用了。
+
+因为 App 不收集大家的用户信息，所以不会有账户权限的限制，请大家自觉遵守：只修改/删除/发布自己的输入方案。
+
+# 上传云端的方案有什么要求
+
+1. 方案需要是自己创作的，且自己有版权的。这一步是为了防止后续不必要的争端。
+2. 为了防止方案之间相互冲突，方案必须遵守一些规则：
+
+    * 方案中不要使用 `rime.lua` 文件，且 `lua` 采用**模块化方式**编写。
+
+    取消 `rime.lua` 的[参考示例](https://github.com/wzxmer/rime-txjx/blob/main/txjx.schema.yaml), 这是 [**天行键**](https://github.com/wzxmer/rime-txjx) 输入方案的配置。
+
+    ```yaml
+    ...
+      translators:
+        - lua_translator@*txjx_calculator
+        - lua_translator@*txjx_time
+        - lua_translator@*txjx_zimu
+    ...
+    ```
+
+    注意：这种新的配置方法，可能不支持 rime 的低版本，如果您有向下兼容的需求，请提供一份仓的专属版本。
+
+    * 方案文件的文件名需要有统一前缀格式，比如[**天行键**](https://github.com/wzxmer/rime-txjx)的方案文件是已 `txjx`开头。
+3. 上传方案的 zip 包不要包含根目录，嵌套一层目录会导致部署失败，已[**天行键**](https://github.com/wzxmer/rime-txjx)的 zip 包制作过程为例: 
+
+```sh
+git clone https://github.com/wzxmer/rime-txjx
+cd rime-txjx
+# 制作云端使用的 zip 文件
+zip -r rime-txjx.zip *
+
+# 查看 zip 文件内容，你会看到 zip 包的目录结构是已用户目录文件为树的根节点的。
+zipinfo -1 rime-txjx.zip
+
+README.md
+default.custom.yaml
+...
+lua/
+lua/txjx_smartTwo.lua
+...
+opencc/
+opencc/SGCharacters.txt
+...
+opencc/txjx.emoji.json
+txjx.custom.yaml
+...
+```
+
+# 云端支持方案（排名不分先后）
+
+* [🍀四叶草拼音输入方案](https://github.com/fkxxyz/rime-cloverpinyin)
+* [86版极点五笔](https://github.com/KyleBing/rime-wubi86-jidian)
+* [星空键道6](https://github.com/xkinput/Rime_JD)
+* [星猫键道百万词库版](https://github.com/wzxmer/xkjd6-rime)
+* [宇浩输入法](https://github.com/forFudan/yuhao)
 
 如有侵权, 请联系我删除. 
-
-# 引用开源项目
-
-* https://github.com/SivanLaai/rime-pure
-* https://github.com/LufsX/rime
-* https://github.com/fkxxyz/rime-cloverpinyin
--- build.sh
+++ build.sh
@@ -4,43 +4,11 @@ set -e
 
 WORK=`pwd`
 
-# 下载已编译过的rimelib
-rime_version=1.8.5
-rime_git_hash=08dd95f
-
-if [[ ! -d .deps ]] 
-then
-  rime_archive="rime-${rime_git_hash}-macOS.tar.bz2"
-  rime_download_url="https://github.com/rime/librime/releases/download/${rime_version}/${rime_archive}"
-
-  rime_deps_archive="rime-deps-${rime_git_hash}-macOS.tar.bz2"
-  rime_deps_download_url="https://github.com/rime/librime/releases/download/${rime_version}/${rime_deps_archive}"
-
-  rm -rf .deps && mkdir -p .deps && (
-      cd .deps
-      [ -z "${no_download}" ] && curl -LO "${rime_download_url}"
-      tar --bzip2 -xf "${rime_archive}"
-      [ -z "${no_download}" ] && curl -LO "${rime_deps_download_url}"
-      tar --bzip2 -xf "${rime_deps_archive}"
-  )
-fi
-
-# # 输入方案临时目录
+# 输入方案临时目录
 OUTPUT=".tmp"
-DST_PATH="$OUTPUT/SharedSupport"
-rm -rf .plum $OUTPUT
-mkdir -p $DST_PATH
-mkdir -p $DST_PATH/lua
-mkdir -p $DST_PATH/opencc
-mkdir -p $DST_PATH/build
-git clone --depth 1 https://github.com/rime/plum.git $OUTPUT/.plum
+DST_PATH="release"
 
-# 可以在这里添加rime的开源输入方案
-# https://github.com/rime/rime-double-pinyin.git
-# for package in essay prelude rime-double-pinyin; do
-for package in prelude; do
-  bash $OUTPUT/.plum/scripts/install-packages.sh "${package}" $DST_PATH
-done
+rm -rf $DST_PATH && mkdir -p $DST_PATH
 
 # 四叶草全拼输入法
 rime_cloverpinyin_version="1.1.4"
@@ -51,147 +19,60 @@ rm -rf $OUTPUT/.clover && mkdir -p $OUTPUT/.clover && (
     [ -z "${no_download}" ] && curl -LO "${rime_cloverpinyin_download_url}"
     unzip "${rime_cloverpinyin_archive}" -d .
     rm -rf ${rime_cloverpinyin_archive}
+    zip -r cloverpinyin.zip ./*
 ) && \
-cp -R $OUTPUT/.clover/*.yaml $DST_PATH/ && \
-cp -R $OUTPUT/.clover/build/* $DST_PATH/build/ && \
-cp -R $OUTPUT/.clover/opencc/* ${DST_PATH}/opencc/
+cp $OUTPUT/.clover/*.zip $OUTPUT
 
-# 内置极点五笔, qq五笔, 小鹤双拼
-#internalSchemas=("wubi86_jidian" "wubi86_qq" "double_pinyin")
-internalSchemas=("double_pinyin")
-for schema in "${internalSchemas[@]}"
-do
-  cp -R Schemas/${schema}/* $DST_PATH/
-done
-
-# 五笔方案
-# 方案来源: https://github.com/networm/Rime
-rm -rf $OUTPUT/.networm && \
-  git clone --depth 1 https://github.com/networm/Rime $OUTPUT/.networm && (
-    cd $OUTPUT/.networm
-    rm -rf .git .gitignore README.md rime.lua default.custom.yaml weasel.custom.yaml
-  ) && cp -R $OUTPUT/.networm/* $DST_PATH/
+# 86版极点五笔
+# https://github.com/KyleBing/rime-wubi86-jidian
+input_scheme_name=rime-wubi86-jidian
+rm -rf $OUTPUT/.$input_scheme_name && \
+  git clone --depth 1 https://github.com/KyleBing/rime-wubi86-jidian $OUTPUT/.$input_scheme_name && (
+    cd $OUTPUT/.$input_scheme_name
+    zip -r $input_scheme_name.zip ./*
+  ) && \
+  cp -R $OUTPUT/.$input_scheme_name/*.zip $OUTPUT
 
 # 星空键道
 # 方案来源: https://github.com/xkinput/Rime_JD
-rm -rf $OUTPUT/.rime_jd && \
-  bash $OUTPUT/.plum/scripts/install-packages.sh xkinput/Rime_JD@plum $OUTPUT/.rime_jd && \
-  cp $OUTPUT/.rime_jd/xkjd6.*.yaml ${DST_PATH} && \
-  cp $OUTPUT/.rime_jd/lua/* ${DST_PATH}/lua/ && \
-  cp $OUTPUT/.rime_jd/opencc/EN2en.* ${DST_PATH}/opencc/ && \
-  cat $OUTPUT/.rime_jd/rime.lua >> ${DST_PATH}/rime.lua
+input_scheme_name=Rime_JD
+rm -rf $OUTPUT/.$input_scheme_name && \
+  git clone -b plum --depth 1 https://github.com/xkinput/$input_scheme_name $OUTPUT/.$input_scheme_name && (
+    cd $OUTPUT/.$input_scheme_name
+    zip -r $input_scheme_name.zip ./*
+  ) && \
+  cp -R $OUTPUT/.$input_scheme_name/*.zip $OUTPUT
 
 # 星猫键道
 # 方案来源: https://github.com/wzxmer/xkjd6-rime
-rm -rf $OUTPUT/.xmjd6 && \
-  git clone --depth 1 https://github.com/wzxmer/xkjd6-rime $OUTPUT/.xmjd6 && (
-    cd $OUTPUT/.xmjd6/xmjd6-Hamster
-    rm -rf README.md default.custom.yaml
+input_scheme_name=xkjd6-rime
+rm -rf $OUTPUT/.$input_scheme_name && \
+  git clone --depth 1 https://github.com/wzxmer/$input_scheme_name $OUTPUT/.$input_scheme_name && (
+    cd $OUTPUT/.$input_scheme_name/xmjd6-Hamster
+    zip -r $input_scheme_name.zip ./*
   ) && \
-  cp $OUTPUT/.xmjd6/xmjd6-Hamster/*.yaml ${DST_PATH} && \
-  cp $OUTPUT/.xmjd6/xmjd6-Hamster/*.txt ${DST_PATH} && \
-  cp $OUTPUT/.xmjd6/xmjd6-Hamster/lua/*.lua ${DST_PATH}/lua/ && \
-  cp $OUTPUT/.xmjd6/xmjd6-Hamster/opencc/* ${DST_PATH}/opencc/ && \
-  cat $OUTPUT/.xmjd6/xmjd6-Hamster/rime.lua >> ${DST_PATH}/rime.lua
+  cp -R $OUTPUT/.$input_scheme_name/xmjd6-Hamster/*.zip $OUTPUT
 
 # 宇浩输入法
 # 方案来源：https://github.com/forFudan/yuhao
-mkdir -p $DST_PATH/yuhao
-rm -rf $OUTPUT/.yuhao && \
-  git clone --depth 1 https://github.com/forFudan/yuhao $OUTPUT/.yuhao && (
-    cd $OUTPUT/.yuhao/dist/yuhao/schema
-    rm -rf default.custom.yaml
-  ) &&  \
-  cp $OUTPUT/.yuhao/dist/yuhao/schema/*.yaml ${DST_PATH} && \
-  cp $OUTPUT/.yuhao/dist/yuhao/schema/yuhao/*.yaml ${DST_PATH}/yuhao/ && \
-  cp -r $OUTPUT/.yuhao/dist/yuhao/schema/lua/* ${DST_PATH}/lua/ && \
-  echo '' >> ${DST_PATH}/rime.lua && \
-  cat $OUTPUT/.yuhao/dist/yuhao/schema/rime.lua >> ${DST_PATH}/rime.lua
+input_scheme_name=yuhao
+rm -rf $OUTPUT/.$input_scheme_name && \
+  git clone --depth 1 https://github.com/forFudan/$input_scheme_name $OUTPUT/.$input_scheme_name && (
+    cd $OUTPUT/.$input_scheme_name/dist/yuhao/schema
+    zip -r $input_scheme_name.zip ./*
+  ) && \
+  cp -R $OUTPUT/.$input_scheme_name/dist/yuhao/schema/*.zip $OUTPUT
 
 # 虎码输入法
 # 方案来源：https://github.com/0ZDragon/rime-huma/tree/main
-rm -rf $OUTPUT/.tiger && \
-	git clone https://github.com/0ZDragon/rime-huma -b main $OUTPUT/.tiger && \
-	cp $OUTPUT/.tiger/* $DST_PATH/
-
-# 绘文字
-# 方案来源: https://github.com/rime/rime-emoji
-rime_emoji_version="15.0"
-rime_emoji_archive="rime-emoji-${rime_emoji_version}.zip"
-rime_emoji_download_url="https://github.com/rime/rime-emoji/archive/refs/tags/${rime_emoji_version}.zip"
-rm -rf $OUTPUT/.emoji && mkdir -p $OUTPUT/.emoji && (
-    cd $OUTPUT/.emoji
-    [ -z "${no_download}" ] && curl -Lo "${rime_emoji_archive}" "${rime_emoji_download_url}"
-    unzip "${rime_emoji_archive}" -d .
-    rm -rf ${rime_emoji_archive}
-    cd rime-emoji-${rime_emoji_version}
-    for target in category word; do
-      ${WORK}/.deps/bin/opencc -c ${WORK}/.deps/share/opencc/t2s.json -i opencc/emoji_${target}.txt > ${target}.txt
-      # workaround for rime/rime-emoji#48
-      # macOS sed 和 GNU sed 不同，见 https://stackoverflow.com/a/4247319/6676742
-      sed -i'.original' -e 's/鼔/鼓/g' ${target}.txt
-      cat ${target}.txt opencc/emoji_${target}.txt | awk '!seen[$1]++' > ../emoji_${target}.txt
-    done
-  ) && \
-cp ${OUTPUT}/.emoji/emoji_*.txt ${DST_PATH}/opencc/ && \
-cp ${OUTPUT}/.emoji/rime-emoji-${rime_emoji_version}/opencc/emoji.json ${DST_PATH}/opencc/
-
-# 整理 DST_PATH 输入方案文件, 生成最终版版本default.yaml
-pushd "${DST_PATH}" > /dev/null
-
-# 减少essay.txt词库
-# awk '($2 >= 500) {print}' essay.txt > essay.txt.min
-# mv essay.txt.min essay.txt
-
-# sed -n '{
-#   s/^version: \(["]*\)\([0-9.]*\)\(["]*\)$/version: \1\2.minimal\3/
-#   /^#以下爲詞組$/q;p
-# }' luna_pinyin.dict.yaml > luna_pinyin.dict.yaml.min
-# mv luna_pinyin.dict.yaml.min luna_pinyin.dict.yaml
-
-# for schema in *.schema.yaml; do
-#   sed '{
-#     s/version: \(["]*\)\([0-9.]*\)\(["]*\)$/version: \1\2.minimal\3/
-#     s/\(- stroke\)$/#\1/
-#     s/\(- reverse_lookup_translator\)$/#\1/
-#   }' ${schema} > ${schema}.min
-#   mv ${schema}.min ${schema}
-# done
-
-# 隐藏五笔方案依赖的pinyin_simp.schema.yaml
-# 隐藏星猫键道方案依赖的xmjd6.dz/cx/W/Y/Z/en
-# 隐藏宇浩输入法依赖的 yuhao_pinyin / yuhao_chaifen / yuhao_chaifen_tw
-ls *.schema.yaml | grep -v pinyin_simp.schema.yaml | grep -v liangfen.schema.yaml \
-  | grep -v xmjd6.en.schema.yaml \
-  | grep -v xmjd6.cx.schema.yaml | grep -v xmjd6.W.schema.yaml \
-  | grep -v xmjd6.Y.schema.yaml | grep -v xmjd6.Z.schema.yaml \
-  | grep -v yuhao_tc.schema.yaml | grep -v yuhao_pinyin.schema.yaml | grep -v yuhao_chaifen.schema.yaml | grep -v yuhao_chaifen_tw.schema.yaml | grep -v yustar_chaifen.schema.yaml \
-  | sed 's/^\(.*\)\.schema\.yaml/  - schema: \1/' > schema_list.yaml
-# 这里不需要只替换luna_pinyin
-# grep -Ff schema_list.yaml default.yaml > schema_list.yaml.min
-# mv schema_list.yaml.min schema_list.yaml
-sed '{
-  s/^config_version: \(["]*\)\([0-9.]*\)\(["]*\)$/config_version: \1\2.minimal\3/
-  /- schema:/d
-  /^schema_list:$/r schema_list.yaml
-}' default.yaml > default.yaml.min
-rm schema_list.yaml
-mv default.yaml.min default.yaml
-
-popd > /dev/null
+# input_scheme_name=rime-huma
+# rm -rf $OUTPUT/.$input_scheme_name && \
+#   git clone --depth 1 https://github.com/0ZDragon/$input_scheme_name $OUTPUT/.$input_scheme_name && (
+#     cd $OUTPUT/.$input_scheme_name
+#     zip -r $input_scheme_name.zip ./*
+#   ) && \
+#   cp -R $OUTPUT/.$input_scheme_name/*.zip $OUTPUT
 
 
 # copy
-cp -R SharedSupport/*.yaml ${DST_PATH}/ 
-
-# TODO: 提前编译
-# export DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:$PWD/.deps/dist/lib
-# .deps/dist/bin/rime_deployer --build ${DST_PATH}
-
-cp -R .deps/share/opencc ${DST_PATH}
-
-# 压缩输入方案
-(
-  cd .tmp
-  zip -r SharedSupport.zip SharedSupport
-) && cp $OUTPUT/*.zip .
+cp -R $OUTPUT/*.zip ${DST_PATH}/ 
